abeec5219ccdf24506ee680f1b4ffe0ca18a5a82
kuba-mazurkiewicz <kmazurki@cisco.com>
2025-01-14 23:08:02 +0100

    add max_timeout

--- main.tf
+++ main.tf
@@ -7,13 +7,14 @@ terraform {
 }
 
 provider "catalystcenter" {
-  username = "username"
-  password = "password"
-  url      = "https://dnac.url"
+  username    = "username"
+  password    = "password"
+  url         = "https://dnac.url"
+  max_timeout = 300
 }
 
 module "catalyst_center" {
-  source  = "git::https://www.github.com/netascode/terraform-catalystcenter-nac-catalystcenter?ref=dev"
+  source = "git::https://www.github.com/netascode/terraform-catalystcenter-nac-catalystcenter?ref=dev"
 
   yaml_directories      = ["data/"]
   templates_directories = ["data/templates/"]

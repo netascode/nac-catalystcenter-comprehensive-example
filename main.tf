183a67403542fac471b5036de3b84683be6b69a5
kuba-mazurkiewicz <kmazurki@cisco.com>
2025-01-30 16:38:16 +0100

    modified timeout and public github

--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ provider "catalystcenter" {
   username    = "username"
   password    = "password"
   url         = "https://dnac.url"
-  max_timeout = 1200
+  max_timeout = 600
 }
 
 module "catalyst_center" {
-  source = "git::https://www.github.com/netascode/terraform-catalystcenter-nac-catalystcenter?ref=dev"
+  source = "git::https://www.github.com/netascode/terraform-catalystcenter-nac-catalystcenter.git"
 
   yaml_directories      = ["data/"]
   templates_directories = ["data/templates/"]
